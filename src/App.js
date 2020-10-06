69674252adc46d95f2affeb138159d6580bfe2ec
Congwen Ma <congwen.ma@rallyhealth.com>
2020-10-05 22:37:26 -0700

    branch 1

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello, World 10
+          Hello, World 11
         </a>
       </header>
     </div>
